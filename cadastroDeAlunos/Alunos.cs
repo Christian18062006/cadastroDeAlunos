96fe1c92b00e7c879a73ad476ee26a9329d190c6
carlos Christian <cursosacessiveis202108@gmail.com>
2024-04-04 01:19:42 -0300

    classe Alunos criada.

---
+++ cadastroDeAlunos/Alunos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cadastroDeAlunos
+{
+    class Alunos
+    {
+        private static int _id;
+        private int _id;
+        public int id()
+        {
+            get
+                {
+                return (_id);
+            }
+        }
+        public string Nome
+        {
+            get; set;
+        }
+        public string Turma
+        {
+            get; set;
+        }
+        public Alunos(string nome, string turma)
+        {
+            id++;
+            _id = id;
+            Nome = nome;
+            Turma = turma;
+        }
+        public override string ToString()
+        {
+            return ($"Id: {id} Nome do aluno: {Nome} \n turma: {Turma} \n");
+        }
+
+    }
+}
+
